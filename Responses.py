120ea49c9ed724b53b75976c0f75ec0854637e47
elyssatanxy <105931363+elyssatanxy@users.noreply.github.com>
2022-07-10 18:06:06 +0800

    set up working db + add budget function

--- Responses.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-con = sqlite3.connect('budgetDatabase.db')
-
-def sql_table(con):
-    cursor = con.cursor
-    cursor.execute("CREATE TABLE BUDGET (CHAT_ID int PRIMARY KEY, BUDGET decimal(5, 2), USERNAME text")
-    con.commit()
-
-sql_table(con)
