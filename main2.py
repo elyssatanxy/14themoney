88211bed1062fc9c00e65125f81bfad555a3651a
elyssatanxy <105931363+elyssatanxy@users.noreply.github.com>
2022-07-11 10:58:35 +0800

    update budget function

--- main2.py
+++ main2.py
@@ -5,6 +5,8 @@ import Constants as keys
 import telegram
 
 bot = telebot.TeleBot(keys.API_KEY)
+global category
+global budget
 
 @bot.message_handler(commands=['start'])
 def welcome_message(message):
@@ -31,12 +33,26 @@ def process_budget(message):
         budget = float(separated[1])
 
         rc = c.execute(f"INSERT OR IGNORE INTO BUDGET VALUES ({username})")
-        # if already exists -> ask if user wants to update
-        rc = c.execute("INSERT INTO CATEGORY ('category_name', 'budget') VALUES (?, ?)", (category, budget))
+        rc = c.execute("INSERT INTO CATEGORY ('category_name', 'budget', 'username') VALUES (?, ?, ?)", (category, budget, username))
         print("Added")
         bot.reply_to(message, f"Added ${budget:.2f} for {category}!")
     except ValueError:
         bot.reply_to(message, "Not a number...")
+    except sqlite3.InternalError:
+        msg = bot.reply_to(message, "You have already created a budget for this category! Do you want to update the budget instead?\nType 'Y' to continue or 'N' to cancel")
+        bot.register_next_step_handler(msg, update_budget)
+
+def update_budget(message):
+    msg = message.text.upper()
+    if msg in "Y":
+        conn = sqlite3.connect(('budgetDatabase.db'))
+        c = conn.cursor()
+        username = message.from_user.id
+        username = str(username)
+
+        rc = c.execute("UPDATE CATEGORY SET budget = ? WHERE category_name = ? AND username = ?", (budget, category, username))
+    elif msg in "N":
+        bot.reply_to(message, "Okay, no changes made!")
 
     conn.commit()
     conn.close
@@ -47,3 +63,4 @@ def process_budget(message):
 
 bot.enable_save_next_step_handlers(delay=0)
 bot.load_next_step_handlers()
+bot.infinity_polling()
