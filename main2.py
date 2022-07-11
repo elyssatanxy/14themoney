379cd7fca66d07e777647cad416001269eb11e96
elyssatanxy <105931363+elyssatanxy@users.noreply.github.com>
2022-07-11 11:23:52 +0800

    updated update budget function + added view function

--- main2.py
+++ main2.py
@@ -5,8 +5,6 @@ import Constants as keys
 import telegram
 
 bot = telebot.TeleBot(keys.API_KEY)
-global category
-global budget
 
 @bot.message_handler(commands=['start'])
 def welcome_message(message):
@@ -29,37 +27,52 @@ def process_budget(message):
 
     try:
         separated = msg.split("-")
+        global category
         category = separated[0]
+        global budget
         budget = float(separated[1])
 
-        rc = c.execute(f"INSERT OR IGNORE INTO BUDGET VALUES ({username})")
-        rc = c.execute("INSERT INTO CATEGORY ('category_name', 'budget', 'username') VALUES (?, ?, ?)", (category, budget, username))
+        c.execute(f"INSERT OR IGNORE INTO BUDGET (username) VALUES ({username})")
+        c.execute("INSERT INTO CATEGORY (category_name, budget, username) VALUES (?, ?, ?)", (category, budget, username))
         print("Added")
         bot.reply_to(message, f"Added ${budget:.2f} for {category}!")
+
+        conn.commit()
+        conn.close
     except ValueError:
         bot.reply_to(message, "Not a number...")
-    except sqlite3.InternalError:
+    except sqlite3.IntegrityError:
         msg = bot.reply_to(message, "You have already created a budget for this category! Do you want to update the budget instead?\nType 'Y' to continue or 'N' to cancel")
         bot.register_next_step_handler(msg, update_budget)
+    except IndexError:
+        bot.reply_to(message, "Didn't quite get that... Please try again.")
 
 def update_budget(message):
     msg = message.text.upper()
     if msg in "Y":
-        conn = sqlite3.connect(('budgetDatabase.db'))
+        conn = sqlite3.connect('budgetDatabase.db')
         c = conn.cursor()
         username = message.from_user.id
         username = str(username)
 
-        rc = c.execute("UPDATE CATEGORY SET budget = ? WHERE category_name = ? AND username = ?", (budget, category, username))
+        c.execute("UPDATE CATEGORY SET budget = ? WHERE category_name = ? AND username = ?", (budget, category, username))
+        bot.reply_to(message, f"Okay, budget for {category} udpated to ${budget}!")
+
+        conn.commit()
+        conn.close
     elif msg in "N":
         bot.reply_to(message, "Okay, no changes made!")
 
-    conn.commit()
-    conn.close
 
-# @bot.message_handler(commands=['view'])
-# def view(message):
-#
+@bot.message_handler(commands=['view'])
+def view(message):
+    conn = sqlite3.connect('budgetDatabase.db')
+    c = conn.cursor()
+    username = message.from_user.id
+    username = str(username)
+
+    c.execute("SELECT * FROM CATEGORY WHERE username = ?", (username,))
+    print(c.fetchone())
 
 bot.enable_save_next_step_handlers(delay=0)
 bot.load_next_step_handlers()
