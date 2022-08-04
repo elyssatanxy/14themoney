0006b1afc31aa94d9a22e01ef6ab28524b0fcb80
elyssatanxy <105931363+elyssatanxy@users.noreply.github.com>
2022-08-05 01:10:21 +0800

    minor changes

--- main.py
+++ main.py
@@ -299,4 +299,4 @@ def process_delete(message):
 
 
 if __name__ == '__main__':
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5432)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
